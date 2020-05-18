02480560628c6ddfcd7d86d04ffe0feeeb25f53c
yawar2019 <yawarali17@gmail.com>
2020-05-18 06:58:27 +0530

    MVC6 am Batch Committed

---
+++ MVC18May2020Batch/MyFirstApp/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public string Index()
+        {
+            return "hello world";
+        }
+
+        public string Contact(string Address)
+        {
+            return Address+" is famous for Hyderabadi Biryani";
+        }
+    }
+}
